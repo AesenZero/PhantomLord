9e8ca9ca84df6e42b7f74941817b14faa6e94441
AesenZero <aesenzero@ukr.net>
2020-07-02 17:48:14 +0300

    SwordAnim

--- Phantom Lord/Assets/_Scripts/Player_Script.cs
+++ Phantom Lord/Assets/_Scripts/Player_Script.cs
@@ -1,26 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
-public class Player_Script : MonoBehaviour
+public class Player_Script : Unit_Script
 {
 
-    public float HP;
-    public float HPmax;
     public float stamina;
     public float STmax;
     public float STregen;
     float timeChecker = 0;
+
+
+    public GameObject weapon; 
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Tick();
+        tempSwing();
     }
 
     public bool CheckST(float t)
@@ -28,7 +33,7 @@ public class Player_Script : MonoBehaviour
         return stamina >= t;
     }
 
-    public void Tick()
+    protected override void Tick()
     {
         timeChecker += Time.deltaTime;
         if (timeChecker >= 1)
@@ -37,4 +42,13 @@ public class Player_Script : MonoBehaviour
             timeChecker = 0;
         }
     }
+
+    public void tempSwing()
+    {
+
+        
+        if (Input.GetKey(KeyCode.Mouse0)) anim.SetBool("Attack_Pressed", true);
+        else anim.SetBool("Attack_Pressed", false);
+    }
+    
 }
